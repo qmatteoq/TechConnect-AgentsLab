6a8abdd1100adf01d9dc8bae1a9318daa724d47b
Matteo Pagani <matteo.pagani@microsoft.com>
2024-12-20 12:25:41 +0100

    Add project files.

---
+++ SingleAgent/Bots/BasicBot.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Agents.Protocols.Adapter;
+using Microsoft.Agents.Protocols.Primitives;
+
+namespace SingleAgent.Bots
+{
+    public class BasicBot: ActivityHandler
+    {
+        protected override async Task OnMessageActivityAsync(ITurnContext<IMessageActivity> turnContext, CancellationToken cancellationToken)
+        {
+            // Create a new Activity from the message the user provided and modify the text to echo back.
+            IActivity message = MessageFactory.Text($"Echo: {turnContext.Activity.Text}");
+
+            // Send the response message back to the user. 
+            await turnContext.SendActivityAsync(message, cancellationToken);
+        }
+
+        protected override async Task OnMembersAddedAsync(IList<ChannelAccount> membersAdded, ITurnContext<IConversationUpdateActivity> turnContext, CancellationToken cancellationToken)
+        {
+            // When someone (or something) connects to the bot, a MembersAdded activity is received.
+            // For this sample,  we treat this as a welcome event, and send a message saying hello.
+            // For more details around the membership lifecycle, please see the lifecycle documentation.
+            IActivity message = MessageFactory.Text("Hello and Welcome! I'm here to help with all your travel needs!");
+
+            // Send the response message back to the user. 
+            await turnContext.SendActivityAsync(message, cancellationToken);
+        }
+    }
+}
